7f835efb7db4ce2695a121feceb6d90c9a237c18
jmslasso <juanmsolisprado@gmail.com>
2023-09-13 18:09:32 -0500

    [FIxing Link]

--- app/components/MovieItems.js
+++ app/components/MovieItems.js
@@ -10,7 +10,7 @@ async function MovieItems() {
         <h1 className="col-span-full text-center text-white text-3xl pb-9">Lista de Shows de TV</h1>
         {data.map((post, index) => {
             return (
-                <Link href= {`/api2/movie/${post.id}`}>
+                <Link as= {`/api2/movie/${post.id}`}>
                     <div className="transition-transform transform hover:scale-105 duration-300 ease-in-out cursor-pointer user-select-none" key={index}>
                         <div className="flex justify-center">
                             <Image className="border 1px solid white" src={`https://image.tmdb.org/t/p/w500/${post.poster_path}`} alt={post.name} width={250} height={300} quality={100} priority={false}/>
