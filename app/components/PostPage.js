acf50c9573a835eeeb3276d8645f8f48666dd980
jmslasso <juanmsolisprado@gmail.com>
2023-09-13 17:58:48 -0500

    [Fix URLs]

--- app/components/PostPage.js
+++ app/components/PostPage.js
@@ -7,7 +7,7 @@ const PostPage =({ posts=[] }) =>{
             <h1 className="text-4xl font-bold mb-4 pt-4 pb-4 text-center">Lista de Posts</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {posts.map((post) => (
-                    <Link href= {`api1/post/${post.id}`}>
+                    <Link href= {`/api1/post/${post.id}`}>
                         <PostDetails key={post.id} post={post} />
                     </Link>
                     
