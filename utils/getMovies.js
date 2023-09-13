23f681e84c759a07e47cf40af79c87a510a5dd8a
jmslasso <juanmsolisprado@gmail.com>
2023-09-13 16:44:30 -0500

    [FIX] adding try and catch

--- utils/getMovies.js
+++ utils/getMovies.js
@@ -1,18 +1,28 @@
 import axios from "axios";
 
 export const FetchMovies = async () => {
-    const key = process.env.TMDB_API_KEY
+  try {
+    const key = process.env.TMDB_API_KEY;
     const response = await axios.get(
-        `https://api.themoviedb.org/3/tv/popular?api_key=${key}`
-      );
+      `https://api.themoviedb.org/3/tv/popular?api_key=${key}`
+    );
     return response.data.results;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
 
 export const FetchMovieDetails = async (id) => {
-    const key = process.env.TMDB_API_KEY
+  try {
+    const key = process.env.TMDB_API_KEY;
     const response = await axios.get(
-        `https://api.themoviedb.org/3/tv/${id}?api_key=${key}`
-      );
-      return response.data;
+      `https://api.themoviedb.org/3/tv/${id}?api_key=${key}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
    
--- utils/getPosts.js
+++ utils/getPosts.js
@@ -1,18 +1,32 @@
 import axios from "axios";
 
 export const getPosts = async() => {
-    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    return data
+    try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        return response.data;
+      } catch (error) {
+        console.error(error);
+        return [];
+      }
 }
 
 export const getPostDetails = async(id) => {
-    const [postRes, commentsRes] = await Promise.all([
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`),
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`),
-      ]);
+    try{
+        const [postRes, commentsRes] = await Promise.all([
+            axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`),
+            axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`),
+          ]);
+        
+        return {
+              post: postRes.data,
+              comments: commentsRes.data,
+        };
+    }
+    catch(error){
+        console.error(error);
+        return null;
+    }
     
-    return {
-          post: postRes.data,
-          comments: commentsRes.data,
-    };
 }
